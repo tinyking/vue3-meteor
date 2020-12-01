4b144b29e26ff71f6c3aefb6c0833021de297826
Jianchao Wang <akingchao@qq.com>
2020-12-01 20:26:48 +0800

    feat: integrate prettier

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,31 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="handleClick">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+	<h1>{{ msg }}</h1>
+	<button @click="handleClick">count is: {{ count }}</button>
+	<p>
+		Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+	</p>
 </template>
 
 <script>
-import {mapGetters} from "vuex";
+	import { mapGetters } from 'vuex';
 
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      // count: 0
-    }
-  },
-  computed: {
-    ...mapGetters(['count'])
-  },
-  methods: {
-    handleClick() {
-      this.$store.dispatch('increment')
-    }
-  }
-}
+	export default {
+		name: 'HelloWorld',
+		props: {
+			msg: String
+		},
+		data() {
+			return {
+				// count: 0
+			};
+		},
+		computed: {
+			...mapGetters(['count'])
+		},
+		methods: {
+			handleClick() {
+				this.$store.dispatch('increment');
+			}
+		}
+	};
 </script>
